91507fc4427ffe788eb65839711981a03c58958b
CoTe <thurakhant@gmail.com>
2024-06-27 23:47:05 +0700

    added services and model

--- lib/generator/flutter_app_generator.dart
+++ lib/generator/flutter_app_generator.dart
@@ -21,15 +21,28 @@ class FlutterAppGenerator {
     for (var feature in features) {
       var featureDir = Directory('${featuresDir.path}/$feature');
       var dataDir = Directory('${featureDir.path}/data');
-      var repoDir = Directory('${dataDir.path}/repo');
+      var repoDir = Directory('${dataDir.path}/repository');
+      var servicesDir = Directory('${dataDir.path}/services');
+      var modelDir = Directory('${dataDir.path}/models');
 
       featureDir.createSync(recursive: true);
       dataDir.createSync(recursive: true);
       repoDir.createSync(recursive: true);
+      servicesDir.createSync(recursive: true);
+      modelDir.createSync(recursive: true);
 
       var repoDartFile = File('${repoDir.path}/${feature}_repository.dart');
+      var serviceDartFile = File('${servicesDir.path}/${feature}_service.dart');
+      var modelDartFile = File('${modelDir.path}/${feature}_model.dart');
+
       repoDartFile.createSync();
       repoDartFile.writeAsStringSync('// Repository for $feature');
+
+      serviceDartFile.createSync();
+      serviceDartFile.writeAsStringSync('// Services for $feature');
+
+      modelDartFile.createSync();
+      modelDartFile.writeAsStringSync('// Model for $feature');
     }
 
     print('Added custom features to Flutter project.');
